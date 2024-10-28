55c38b2b9db788f91174f1431740485d06d09186
collintegral <collintegral@gmail.com>
2024-10-28 17:49:56 -0400

    Improved header/footer links

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,11 +1,11 @@
 import { loadPartial } from "./utilities.mjs";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    await loadPartial("./public/partials/header.html", "header");
+    await loadPartial("../public/partials/header.html", "header");
     
     /* Potential hamburger if sitenav grows unwieldy: 
     const menuButton = document.getElementById('menubtn');
     menuButton.addEventListener('click', menuClick); */
 
-    loadPartial("./public/partials/footer.html", "footer");
+    loadPartial("../public/partials/footer.html", "footer");
 });
